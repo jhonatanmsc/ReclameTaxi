4bb705493b8f6fdc40421cf1d18588891a2d522c
jhonatan <jhonatan@retake.com.br>
2019-06-10 10:29:02 -0300

    ADD: adicionada view para login recebendo dados de authenticação oauth

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import authenticate
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from apps.user.models import User
+
+
+def AuthLogin(request):
+
+    userJSON = request.data
+    user, created = User.objects.get_or_create(
+        uid=userJSON['uid'],
+        photo=userJSON['photoURL'],
+        provider=userJSON['providerID'],
+        nome=userJSON['displayName'],
+        email=userJSON['email'],
+        telefone=userJSON['phoneNumber']
+    )
+
+    user.set_password('Informatica')
+    user.save()
+    authenticate(username=user.nome, password='Informatica')
+    responseJSON = serializers.serialize('json', user)
+    return HttpResponse(responseJSON, mimetype='application/json')
+
